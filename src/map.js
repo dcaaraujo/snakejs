c25c30a44997d9e9edd85d6e2cc5010b47127bc0
Christopher Araujo <78472544+dcaaraujo@users.noreply.github.com>
2024-04-17 17:34:39 +0200

    Reduced gameboard size

--- src/map.js
+++ src/map.js
@@ -1,8 +1,8 @@
 import { getRandomInt } from "./utils";
 
 export const TILE_SIZE = 25;
-export const NUM_COLUMNS = 30;
-export const NUM_ROWS = 25;
+export const NUM_COLUMNS = 20;
+export const NUM_ROWS = 15;
 
 export function getRandomPosition() {
   const x = getRandomInt(0, NUM_COLUMNS);
